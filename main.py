d29fb8dd644a9ab5b237a44270296db4a91df7ef
sansanxiansheng <39691594+sansanxiansheng@users.noreply.github.com>
2025-08-14 10:00:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -67,8 +67,8 @@ def refresh_cookie():
         push(ERROR_CODE, PUSH_METHOD)
         raise Exception(ERROR_CODE)
         
-min_seconds = 0.1 * 60   # 0.1 h
-max_seconds = 2 * 60   # 2 h
+min_seconds = 0.1 * 60 * 60    # 0.1 h
+max_seconds = 2 * 60 * 60    # 2 h
 wait_time_seconds = random.randint(min_seconds, max_seconds)
 logging.info(f"Waiting for a random duration of {wait_time_seconds / 3600:.2f} hours ({wait_time_seconds} seconds)...")
 time.sleep(wait_time_seconds)
